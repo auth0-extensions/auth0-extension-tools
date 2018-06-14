57cb2bc2c66b45390959914ce37f7cc4afeba88f
zxan1285 <nonexist.void@gmail.com>
2018-06-14 10:12:43 +0300

    no stack trace for production

--- src/errors/ArgumentError.js
+++ src/errors/ArgumentError.js
@@ -1,6 +1,8 @@
 function ArgumentError(message) {
   Error.call(this, message);
-  Error.captureStackTrace(this, this.constructor);
+  if (process.env.NODE_ENV !== 'production') {
+    Error.captureStackTrace(this, this.constructor);
+  }
   this.name = 'ArgumentError';
   this.message = message;
   this.status = 400;
--- src/errors/HookTokenError.js
+++ src/errors/HookTokenError.js
@@ -1,6 +1,8 @@
 function HookTokenError(message, innerError) {
   Error.call(this, message);
-  Error.captureStackTrace(this, this.constructor);
+  if (process.env.NODE_ENV !== 'production') {
+    Error.captureStackTrace(this, this.constructor);
+  }
   this.name = 'HookTokenError';
   this.message = message;
   this.status = 401;
